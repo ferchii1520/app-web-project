87d7c5e4a83a8e504b888eaaa9885ae5d6f1adbc
Fernanda Fuentes <fuentesdiazmafernanda@gmail.com>
2024-06-30 21:12:07 -0600

    Inital Commit

---
+++ app-web-project/src/main/java/com/keepcoding/app/web/AppWebProyectApplication.java
@@ -0,0 +1,13 @@
+package com.keepcoding.app.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebProyectApplication.class, args);
+	}
+
+}
